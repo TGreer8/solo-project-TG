5a668c6a9b579939a3fdfdae3c96ab23c86fd7f5
Taylor Greer <taylorbgreer@gmail.com>
2022-03-26 00:02:58 -0400

    This is it

--- f1Test.test.ts
+++ f1Test.test.ts
@@ -1,1 +1,64 @@
-// This is where my test will go. 
+import {f1Page} from "./pageObjects"
+
+
+
+const myTest = new f1Page
+
+
+   
+    test("Drivers Tab", async () =>{
+        await myTest.navigate()
+        await myTest.driver.manage().window().maximize()
+        await myTest.click(myTest.cookiesBtn)
+       
+        await myTest.click(myTest.driverTab)
+        await myTest.click(myTest.exitBtn)
+        await myTest.click(myTest.danielRicci)
+        let response = await myTest.getText(myTest.driverResult)
+        expect(response).toContain( "Daniel Ricciardo")
+        
+    
+    });
+    
+    test("Teams Tab", async () => {
+        await myTest.navigate()
+        await myTest.click(myTest.exitBtn)
+        await myTest.click(myTest.teamsTab)
+        
+        await myTest.click(myTest.ferrari)
+        let response = await myTest.getText(myTest.teamResult)
+        
+        expect(response).toContain("Ferrari")
+    
+    
+    });
+
+    test("Schedule Tab", async () => {
+       // await myTest.navigate()
+        await myTest.click(myTest.scheduleTab)
+        await myTest.click(myTest.spain)
+        let response = await myTest.getText(myTest.scheduleResult)
+        expect(response).toContain("FORMULA 1 PRE-SEASON TRACK SESSION 2022")
+      
+     
+    });
+
+    test("Sign in", async () => {
+        await myTest.navigate() 
+       
+        
+        await myTest.click(myTest.signInBtn)
+       
+        await myTest.click(myTest.emailInput)
+        await myTest.setInput(myTest.emailInput, "taylorbgreer@gmail.com")
+        await myTest.click(myTest.passwordInput)
+        await myTest.setInput(myTest.passwordInput, "Test123!")
+        await myTest.click(myTest.signInBtn2)
+        let response = await myTest.getText(myTest.signInresult)
+        expect(response).toContain("Sorry something went wrong")
+        await myTest.driver.quit()
+    });
+
+
+    
+    
--- pageObjects.ts
+++ pageObjects.ts
@@ -1,1 +1,41 @@
-//this is just a test I think. 
+import { By, WebDriver } from "selenium-webdriver"; 
+import {BasePage} from "./basePage" 
+
+export class f1Page extends BasePage {
+    driver: WebDriver
+    url: string
+
+    cookiesBtn: By = By.xpath('(//button[@class="trustarc-agree-btn"])')
+    exitBtn: By = By.xpath('//button[@class="sailthru-overlay-close"]')
+    driverTab: By = By.xpath('(//span[@class="link-text"]) [5]')
+    danielRicci: By = By.xpath('(//div[@class="listing-item--image-wrapper f1-pattern-fill"]) [14]')
+    driverResult: By = By.xpath('(//h1[@class="driver-name"])')
+   
+    teamsTab: By = By.xpath('(//span[@class="link-text"]) [6]')
+    ferrari: By = By.xpath('(//div[@class="listing-info"]) [1]')
+    teamResult: By = By.xpath('(//h1[@class="headline"])')
+   
+    scheduleTab: By = By.xpath('(//span[@class="link-text"]) [3]')
+    spain: By = By.xpath('(//div[@class="event-place"]) [1]') 
+    scheduleResult: By = By.xpath('(//h2[@class="f1--s"])')
+
+    signInBtn: By = By.xpath('(//span[@class="user-icon-text"]) [1]')
+    emailInput: By = By.xpath('//input[@name="Login"]')
+    passwordInput: By = By.xpath('//input[@name="Password"]')
+    signInBtn2: By = By.xpath('(//button[@class="btn btn-primary"]) [1]')
+    signInresult: By = By.xpath('(//div[@class="field alert alert-danger alert-error"])')
+
+    accountBtn: By = By.xpath('(//a[@class="user-account-actions btn btn--default btn--icon-left btn--transparent"])')
+    logoutBtn: By = By.xpath('(//a[@class="btn btn--alpha logout-link"])')
+
+    manageAccountBtn: By = By.xpath('(//a[@class="btn btn--default my-account"])')
+    accountResults: By = By.xpath('(//div[@class="panel-heading"])')
+    
+
+
+
+
+    constructor() {
+        super({url: 'https://www.formula1.com/en.html'})
+    }
+}
